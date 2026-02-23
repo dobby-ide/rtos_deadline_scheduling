d0577d3d2f7f74de327c53f5441659b52575b9ca
Fabio <fabio_privitera@yahoo.it>
2026-02-23 13:03:54 +0200

    adds example dev testing

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        // example developmental testing
+        List<Task> tasks = new ArrayList<>();
+
+
+        Scheduler scheduler = new DMScheduler();
+
+        Simulator simulator = new Simulator(tasks, scheduler, 20);
 
     }
 }
