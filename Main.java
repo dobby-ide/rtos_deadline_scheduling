7b4b6a991451699a93afef0eadb5ee537ee10311
Fabio <fabio_privitera@yahoo.it>
2026-02-23 15:11:46 +0200

    testing in a tick based enironment

--- Main.java
+++ Main.java
@@ -5,11 +5,15 @@ public class Main {
     public static void main(String[] args) {
         // example developmental testing
         List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task(1, 0, 8, 12, 10)); // executionTime > period
+        tasks.add(new Task(2, 0, 10, 20, 15));
 
 
-        Scheduler scheduler = new DMScheduler();
+        Scheduler scheduler = new RMScheduler();
 
         Simulator simulator = new Simulator(tasks, scheduler, 20);
+
+        simulator.run();
 
     }
 }
