1278836e35ed18f160e247045dd8e6e433677284
Fabio <fabio_privitera@yahoo.it>
2026-02-23 18:36:00 +0200

    implements logic for the algorithm

--- DMScheduler.java
+++ DMScheduler.java
@@ -1,8 +1,11 @@
+import java.util.Comparator;
 import java.util.List;
 
 public class DMScheduler implements Scheduler{
     @Override
     public Job selectNextJob(List<Job> readyQueue, int currentTime) {
-        return null;
+        return readyQueue.stream()
+                .min(Comparator.comparingInt(job -> job.getTaskRelativeDeadline()))
+                .orElse(null);
     }
 }
--- EDFScheduler.java
+++ EDFScheduler.java
@@ -1,8 +1,11 @@
+import java.util.Comparator;
 import java.util.List;
 
 public class EDFScheduler implements Scheduler{
     @Override
     public Job selectNextJob(List<Job> readyQueue, int currentTime) {
-        return null;
+        return readyQueue.stream()
+                .min(Comparator.comparingInt(Job::getAbsoluteDeadline))
+                .orElse(null);
     }
 }
